2762ca8b0f2a46a3db01929417dab18c5fce1b46
Clint Brown <c.brown@westnet.com.au>
2015-01-12 15:23:11 +1100

    Add app_data

--- youtube/index.php
+++ youtube/index.php
@@ -145,9 +145,9 @@
 
       $data = implode(",", $signed_request);
       echo $data;
-      echo "0 " . $data[0];
-      echo "1 " . $data[1];
-      echo "2 " . $data[2];
+      echo "0 " . $signed_request[0];
+      echo "1 " . $signed_request[1];
+      echo "2 " . $signed_request[2];
       //$signed_request['app_data'] = MYDATA;
 
     ?>
