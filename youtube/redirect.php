2d4682075513c8cee9284c48c7bdffeb76f15d1b
Clint Brown <c.brown@westnet.com.au>
2015-01-12 15:45:28 +1100

    Add app_data

--- youtube/redirect.php
+++ youtube/redirect.php
@@ -1,3 +1,19 @@
 <script>
-  top.location="https://www.facebook.com/pagestestappdigitaslbi/app_361939297298147";
+  (function () {
+    var param,
+      appData = '';
+
+    function gup (name) {
+      name = RegExp ('[?&]' + name.replace (/([[\]])/, '\\$1') + '=([^&#]*)');
+      return (window.location.href.match (name) || ['', ''])[1];
+    }
+
+    param = gup('app_data');
+
+    if (param) {
+      appData = '?app_data=' + param;
+    }
+
+    return top.location = 'https://www.facebook.com/pagestestappdigitaslbi/app_361939297298147' + appData;
+  })();
 </script>
