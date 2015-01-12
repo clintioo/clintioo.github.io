ac97f24117b10a59c2cf4ffe80f949a7d5b3f47c
Clint Brown <c.brown@westnet.com.au>
2015-01-12 14:45:26 +1100

    Add app_data

--- youtube/index.php
+++ youtube/index.php
@@ -112,7 +112,39 @@
 </head>
 <body>
   <div class="youtube">
-    <?php print_r($_REQUEST) ?>
+    <?php
+
+      function parse_signed_request($signed_request, $secret) {
+        list($encoded_sig, $payload) = explode('.', $signed_request, 2); 
+
+        // decode the data
+        $sig = base64_url_decode($encoded_sig);
+        $data = json_decode(base64_url_decode($payload), true);
+
+        if (strtoupper($data['algorithm']) !== 'HMAC-SHA256') {
+            error_log('Unknown algorithm. Expected HMAC-SHA256');
+            return null;
+        }
+
+        // check sig
+        $expected_sig = hash_hmac('sha256', $payload, $secret, $raw = true);
+        if ($sig !== $expected_sig) {
+            error_log('Bad Signed JSON signature!');
+            return null;
+        }
+
+        return $data;
+      }
+
+      function base64_url_decode($input) {
+        return base64_decode(strtr($input, '-_', '+/'));
+      }
+
+      $req = $_REQUEST;
+      echo $req;
+      echo $req[signed_request];
+
+    ?>
 
     <div class="youtube__overlay"></div>
   </div>
